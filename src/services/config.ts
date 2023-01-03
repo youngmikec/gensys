2ac43354fad3ea99b07cedc82ee239a9fb214bd6
youngmikec <michaelozor15@gmail.com>
2023-01-03 21:09:15 +0100

    fixed base url

--- src/services/config.ts
+++ src/services/config.ts
@@ -3,8 +3,8 @@ import { getItem } from '../utils';
 
 const token = getItem('clientToken');
 // export const url: string = 'https://generate-api.onrender.com/api';
-// export const url: string | undefined = process.env.REACT_APP_BASE_URL;
-export const url: string = "http://localhost:3000/api";
+export const url: string | undefined = process.env.REACT_APP_BASE_URL;
+// export const url: string = "http://localhost:3000/api";
 axios.defaults.baseURL = url;
 axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 axios.defaults.headers.common['Content-Type'] = `application/json`;
